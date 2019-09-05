ef1392aa555d0134aed4210fa7f096931dc61a77
Drew McPherson <drewmac1971@gmail.com>
2019-09-04 22:14:47 -0500

    added python numbers practice file

---
+++ Numbers.py
@@ -0,0 +1,33 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3 + 4
+7
+>>> 3 * 20
+60
+>>> 12 / 4
+3.0
+>>> 8 + 2 * 10
+28
+>>> (8 + 2) * 10
+100
+>>> 18 / 4
+4.5
+>>> 18//4
+4
+>>> 18%4
+2
+>>> 5 * 5 * 5
+125
+>>> 5 ** 5
+3125
+>>> 5 ** 3
+125
+>>> tuna = 5
+>>> 20 + tuna
+25
+>>> bacon = 18
+>>> bacon + tuna
+23
+>>> bacon / tuna
+3.6
+>>> 
