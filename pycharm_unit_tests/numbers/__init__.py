4a15bb7e3869f18c9ed1718897d62452066ecc7f
Drew McPherson <drewmac1971@gmail.com>
2019-09-15 21:13:37 -0500

    added more unit tests for add, diff, mult, and quot

--- pycharm_unit_tests/numbers/__init__.py
+++ pycharm_unit_tests/numbers/__init__.py
@@ -5,8 +5,33 @@ def sum(arg):
     return total
 
 
+def diff(arg):
+    total = 0
+    for val in arg:
+        total -= val
+    return total
+
+
 def prod(arg):
     total = 1
     for val in arg:
         total *= val
     return total
+
+
+def quot(arg):
+    total = 100
+    for val in arg:
+        total /= val
+    return total
+
+
+def quot_large(arg):
+    total = 4829587185
+    for val in arg:
+        total /= val
+    return total
+
+
+
+
