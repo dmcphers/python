2e955f213de5b6567eb6b725783d431b55793d43
Drew McPherson <drewmac1971@gmail.com>
2019-09-17 10:42:35 -0500

    adding more string unit tests

--- pycharm_unit_tests/test_strings_unittest.py
+++ pycharm_unit_tests/test_strings_unittest.py
@@ -10,6 +10,36 @@ class TestStrings(unittest.TestCase):
         result = concat(data)
         self.assertEqual(result, "Drew McPherson")
 
+    def test_st_alphanum_concat(self):
+        data = ["Drew ", "2019 ", "McPherson"]
+        result = concat(data)
+        self.assertEqual(result, "Drew 2019 McPherson")
+
+    def test_st_num_concat(self):
+        data = ["9 ", "16 ", "2019"]
+        result = concat(data)
+        self.assertEqual(result, "9 16 2019")
+
+    def test_st_specl_concat(self):
+        data = ["Drew ", "????? ", "McPherson ", "!!!!!"]
+        result = concat(data)
+        self.assertEqual(result, "Drew ????? McPherson !!!!!")
+
+    def test_st_apostrophe(self):
+        data = ["I don't think so"]
+        result = concat(data)
+        self.assertEqual(result, "I don't think so")
+
+    def test_st_quote(self):
+        data = ['He said, "I see it"']
+        result = concat(data)
+        self.assertEqual(result, 'He said, "I see it"')
+
+    def test_st_escape_char(self):
+        data = ['I don\'t think so']
+        result = concat(data)
+        self.assertEqual(result, "I don't think so")
+
 
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
