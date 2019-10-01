abb1b034cab2a02de66fd832bdbed48eaa2b6f67
Drew McPherson <drewmac1971@gmail.com>
2019-09-30 21:59:40 -0500

    added more tests to test_basic_math_operations_unittest.py

--- pycharm_unit_tests/test_basic_math_operations_unittest.py
+++ pycharm_unit_tests/test_basic_math_operations_unittest.py
@@ -1,23 +1,6 @@
 import unittest
 
 
-class TestBasicMathOperations(unittest.TestCase):
-	def setUp(self):
-		self.bmo = BasicMathOperations()
-
-	def test_add(self):
-		self.assertEqual(self.bmo.add(3, 4), 7)
-
-	def test_subtract(self):
-		self.assertEqual(self.bmo.subtract(4, 3), 1)
-
-	def test_multiply(self):
-		self.assertEqual(self.bmo.multiply(3, 4), 12)
-
-	def test_divide(self):
-		self.assertEqual(self.bmo.divide(20, 4), 5)
-
-
 # Class to handle basic arithmetic operations
 class BasicMathOperations:
 
@@ -58,6 +41,117 @@ class BasicMathOperations:
 		self.y = y
 		self.difference = self.x - self.y
 		return self.difference
+
+
+class TestBasicMathOperations(unittest.TestCase):
+	def setUp(self):
+		self.bmo = BasicMathOperations()
+
+	def test_add_pos(self):
+		self.assertEqual(self.bmo.add(3, 4), 7)
+
+	def test_add_neg(self):
+		self.assertEqual(self.bmo.add(-3, -4), -7)
+
+	def test_add_pos_neg(self):
+		self.assertEqual(self.bmo.add(-3, 4), 1)
+
+	def test_add_zero(self):
+		self.assertEqual(self.bmo.add(0, 4), 4)
+
+	# method fails because 2 values are required per the class definition
+	# def test_add_value_missing(self):
+	# 	self.assertEqual(self.bmo.add(3,), 3)
+
+	# method fails because 2 values are required per the class definition
+	# def test_add_none(self):
+	# 	self.assertEqual(self.bmo.add(), )
+
+	def test_add_large_pos_nums(self):
+		self.assertEqual(self.bmo.add(4738494329, 7529383821), 12267878150)
+
+	def test_add_large_neg_nums(self):
+		self.assertEqual(self.bmo.add(-4738494329, -7529383821), -12267878150)
+
+	def test_subtract_pos(self):
+		self.assertEqual(self.bmo.subtract(4, 3), 1)
+
+	def test_subtract_neg(self):
+		self.assertEqual(self.bmo.subtract(-4, -3), -1)
+
+	def test_subtract_pos_neg(self):
+		self.assertEqual(self.bmo.subtract(-4, 3), -7)
+
+	def test_subtract_zero(self):
+		self.assertEqual(self.bmo.subtract(0, 3), -3)
+		self.assertEqual(self.bmo.subtract(3, 0), 3)
+
+	# method fails because 2 values are required per the class definition
+	# def test_subtract_value_missing(self):
+	# 	self.assertEqual(self.bmo.subtract(4), 4)
+
+	# method fails because 2 values are required per the class definition
+	# def test_subtract_none(self):
+	# 	self.assertEqual(self.bmo.subtract(), )
+
+	def test_subtract_pos_largenums(self):
+		self.assertEqual(self.bmo.subtract(7529383821, 4738494329), 2790889492)
+
+	def test_subtract_neg_largenums(self):
+		self.assertEqual(self.bmo.subtract(-7529383821, -4738494329), -2790889492)
+
+	def test_multiply_pos(self):
+		self.assertEqual(self.bmo.multiply(3, 4), 12)
+
+	def test_multiply_neg(self):
+		self.assertEqual(self.bmo.multiply(-3, -4), 12)
+
+	def test_multiply_pos_neg(self):
+		self.assertEqual(self.bmo.multiply(3, -4), -12)
+
+	def test_multiply_zero(self):
+		self.assertEqual(self.bmo.multiply(0, 4), 0)
+
+	# method fails because 2 values are required per the class definition
+	# def test_multiply_value_missing(self):
+	# 	self.assertEqual(self.bmo.multiply(3,), 0)
+
+	# method fails because 2 values are required per the class definition
+	# def test_multiply_none(self):
+	# 	self.assertEqual(self.bmo.multiply(), )
+
+	def test_multiply_pos_largenums(self):
+		self.assertEqual(self.bmo.multiply(383821, 494329), 189733851109)
+
+	def test_multiply_neg_largenums(self):
+		self.assertEqual(self.bmo.multiply(383821, -494329), -189733851109)
+
+	def test_divide_pos(self):
+		self.assertEqual(self.bmo.divide(20, 4), 5)
+
+	def test_divide_neg(self):
+		self.assertEqual(self.bmo.divide(-20, -4), 5)
+
+	def test_divide_pos_neg(self):
+		self.assertEqual(self.bmo.divide(20, -4), -5)
+
+	# method fails because of division by zero error
+	# def test_divide_zero(self):
+	# 	self.assertEqual(self.bmo.divide(20, 0), 5)
+
+	# method fails because 2 values are required per the class definition
+	# def test_divide_value_missing(self):
+	# 	self.assertEqual(self.bmo.divide(20,), 5)
+
+	# method fails because 2 values are required per the class definition
+	# def test_divide_none(self):
+	# 	self.assertEqual(self.bmo.divide(), )
+
+	def test_divide_pos_largenums(self):
+		self.assertEqual(self.bmo.divide(4829587185, 5), 965917437)
+
+	def test_divide_neg_largenums(self):
+		self.assertEqual(self.bmo.divide(4829587185, -5), -965917437)
 
 
 if __name__ == '__main__':
--- pycharm_unit_tests/test_numbers_unittest.py
+++ pycharm_unit_tests/test_numbers_unittest.py
@@ -6,22 +6,22 @@ from numbers import summation, prod, diff, quot, quot_large
 class TestNumbers(unittest.TestCase):
 
     def test_sum_pos(self):
-        data = [3,4]
+        data = [3, 4]
         result = sum(data)
         self.assertEqual(result, 7)
 
     def test_sum_neg(self):
-        data = [-3,-4]
+        data = [-3, -4]
         result = sum(data)
         self.assertEqual(result, -7)
 
     def test_sum_pos_neg(self):
-        data = [-3,4]
+        data = [-3, 4]
         result = sum(data)
         self.assertEqual(result, 1)
 
     def test_sum_zero(self):
-        data = [0,4]
+        data = [0, 4]
         result = sum(data)
         self.assertEqual(result, 4)
 
@@ -36,27 +36,27 @@ class TestNumbers(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_sum_largenums(self):
-        data = [4738494329,7529383821]
+        data = [4738494329, 7529383821]
         result = sum(data)
         self.assertEqual(result, 12267878150)
 
     def test_diff_pos(self):
-        data = [-13,4]
+        data = [-13, 4]
         result = diff(data)
         self.assertEqual(result, 9)
 
     def test_diff_neg(self):
-        data = [3,-4]
+        data = [3, -4]
         result = diff(data)
         self.assertEqual(result, 1)
 
     def test_diff_pos_neg(self):
-        data = [-3,4]
+        data = [-3, 4]
         result = diff(data)
         self.assertEqual(result, -1)
 
     def test_diff_zero(self):
-        data = [0,4]
+        data = [0, 4]
         result = diff(data)
         self.assertEqual(result, -4)
 
@@ -71,27 +71,27 @@ class TestNumbers(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_diff_largenums(self):
-        data = [-7529383821,4738494329]
+        data = [-7529383821, 4738494329]
         result = diff(data)
         self.assertEqual(result, 2790889492)
 
     def test_prod_pos(self):
-        data = [3,20]
+        data = [3, 20]
         result = prod(data)
         self.assertEqual(result, 60)
 
     def test_prod_neg(self):
-        data = [-3,-20]
+        data = [-3, -20]
         result = prod(data)
         self.assertEqual(result, 60)
 
     def test_prod_pos_neg(self):
-        data = [-3,20]
+        data = [-3, 20]
         result = prod(data)
         self.assertEqual(result, -60)
 
     def test_prod_zero(self):
-        data = [0,4]
+        data = [0, 4]
         result = prod(data)
         self.assertEqual(result, 0)
 
@@ -106,12 +106,12 @@ class TestNumbers(unittest.TestCase):
         self.assertEqual(result, 1)
 
     def test_prod_largenums(self):
-        data = [383821,494329]
+        data = [383821, 494329]
         result = prod(data)
         self.assertEqual(result, 189733851109)
 
     def test_quot_pos(self):
-        data = [2,10]
+        data = [2, 10]
         result = quot(data)
         self.assertEqual(result, 5)
 
@@ -121,7 +121,7 @@ class TestNumbers(unittest.TestCase):
         self.assertEqual(result, 5)
 
     def test_quot_pos_neg(self):
-        data = [-2,10]
+        data = [-2, 10]
         result = quot(data)
         self.assertEqual(result, -5)
 
