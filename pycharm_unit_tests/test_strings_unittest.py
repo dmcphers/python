d7fbfc3e35f59900da9b5d1d162a9c6085b79532
Drew McPherson <drewmac1971@gmail.com>
2019-11-24 12:39:40 -0600

    modified test_string_unittest.py

--- pycharm_unit_tests/test_strings_unittest.py
+++ pycharm_unit_tests/test_strings_unittest.py
@@ -2,6 +2,23 @@ import unittest
 
 from strings import concat, st_multiply, st_slice, st_length
 
+# def concat(arg):
+#     st_concat = ""
+#     for val in arg:
+#         st_concat += val
+#     return st_concat
+#
+# def st_multiply(arg):
+#     st_multiply = arg * 5
+#     return st_multiply
+#
+# def st_slice(arg):
+#     st_slice_st = arg[:7]
+#     return st_slice_st
+#
+# def st_length(arg):
+#     st_len = len(arg)
+#     return st_len
 
 class TestStrings(unittest.TestCase):
 
