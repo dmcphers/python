f13a20c9aa732f0513cc219016051dad496c777d
Drew McPherson <drewmac1971@gmail.com>
2019-11-24 11:59:40 -0600

    cleaning up strings.py practice file

--- Strings.py
+++ Strings.py
@@ -30,4 +30,28 @@ C:\Drew\Desktop\nPics
 'Drew McBrew'
 >>> firstName * 5
 'Drew Drew Drew Drew Drew '
->>> 
+>>>
+>>> user = "Tuna McFish"
+>>> user[0]
+'T'
+>>> user[5]
+'M'
+>>> user[-1]
+'h'
+>>> user[-3]
+'i'
+>>> user[2:7]
+'na Mc'
+>>> user[:7]
+'Tuna Mc'
+>>> user[2:]
+'na McFish'
+>>> user[:]
+'Tuna McFish'
+>>> print('sdfsdflj')
+sdfsdflj
+>>> len('asdfa;dfjlfafldjfl')
+18
+>>> len(user)
+11
+>>>
